de861ee9bc02d97c67236de8e782beedcc17f90d
17hariram <1707hariram@gmail.com>
2024-12-05 02:11:13 +0530

    Operators code

---
+++ Operators.java
@@ -0,0 +1,14 @@
+public class Operators {
+    public static void main (String args[])
+    {
+        int a=10, b=20;
+        if (a>b)
+        {
+            System.out.println("The condition is false");
+        }
+        else
+        {
+            System.out.println("The system is true");
+        }
+    }
+}
