f7adea002202de7c9a3917caf5c43b4b81bae943
17hariram <1707hariram@gmail.com>
2024-12-05 02:02:16 +0530

    Datatypes, Variables

---
+++ Datatypes.java
@@ -0,0 +1,9 @@
+public class Datatypes {
+    public static void main(String args[])
+    {
+       int a=2;
+       float b = 9.72f;
+       double c= 73.495;
+       System.out.println(a+b+c);
+    }
+}
